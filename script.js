9c7adfb0fffcf73bd6704e261d6a1a2201962a62
phil <philip.barwood@gmail.com>
2017-12-04 01:06:41 +0000

    updates

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
+/* written by P.B. - 2017 */
+
+var s;
 var c;
 var ctx;
 var map;
 	
-var P = 6 //pixel size
-var W;
-var H;
+var P = 8; //pixel size
+var interval = 50; //tick interval millisecs
 var wCount;
 var hCount;
 
+function onSlider(){
+	interval = s.value * 10;
+}
+
 window.onload = function(){
 	document.body.style.overflow = "hidden";
-	W = window.innerWidth - 20;
-	H = window.innerHeight - 20;
-	wCount = Math.ceil(W/P)+1;
-	hCount = Math.ceil(H/P)+1;
+	var W = window.innerWidth - 18;
+	var H = window.innerHeight - 18;
+	wCount = Math.ceil(W/P)-1;
+	hCount = Math.ceil(H/P)-1;
 	c = document.createElement("canvas");
-	c.width = W;
-	c.height = H;
+	c.width = wCount * P;
+	c.height = hCount * P;
+	c.style.display = "block";
+	c.style.margin = "auto";
 	c.style.border = "1px solid #d3d3d3";
 	ctx = c.getContext("2d");
 // 	c.addEventListener('dblclick', toggleClock, true);
@@ -26,7 +34,29 @@ window.onload = function(){
 	document.body.addEventListener('keydown', keyDown, true);
 	document.body.appendChild(c);
 	initMap();
+	// var w = new worker;
+	createSlider();
+	info();
 }
+
+function createSlider(){
+	s = document.createElement("input");
+	s.type = "range";
+	s.min = 0;
+	s.max = 50;
+	s.value = 5; 
+	s.style.position = "fixed";
+	s.style.bottom = "18px";
+ 	s.style.width = "80%";
+  	s.style.marginLeft = "10%"; 
+	document.body.appendChild(s);	
+	s.addEventListener('input', onSlider, true);
+}
+
+function info(){
+	alert("Start/Stop - Space\nDraw - Left Click\nReset - Right Click");
+}
+
 
 function keyDown(e){
 	if(e.keyCode==32){
@@ -51,8 +81,10 @@ function mouseUp(e){
 	c.removeEventListener('mousemove', addPixel, true);
 }
 
+// var tickCount;
 var cid;
 function toggleClock(){
+// 	tickCount = 0;
 	cid = cid ? window.clearTimeout(cid) : onTick();
 };
 
@@ -60,8 +92,8 @@ var flag = true;
 function onTick(){
 	evolve();
 	drawMap();
-	var interval = 20; //tick interval millisecs
 	cid = window.setTimeout(onTick, interval);
+// 	if(++tickCount==300){toggleClock()};
 }
 
 function initMap(){
@@ -81,58 +113,58 @@ function initMap(){
 
 function reset(e){
 	cid && toggleClock();
-	map.each((x,y)=>{map[x][y]=0});
+	map.each((x,y)=>{map[x][y]=1 });
 	drawMap();	
 }
 
 function pressure(x,y){
 	var count = 0;
-	if(x>0){
-		map[x-1][y-1] % 2 && ++count;
-		map[x-1][y]   % 2 && ++count;
-		map[x-1][y+1] % 2 && ++count;
+	if(y==0||y==hCount-1||x==0||x==wCount-1){
+		return -1;
 	}
+	map[x-1][y-1] % 2 && ++count;
+	map[x-1][y]   % 2 && ++count;
+	map[x-1][y+1] % 2 && ++count;
 	map[x][y-1] % 2 && ++count;
 	map[x][y+1] % 2 && ++count;
-	if(x<wCount-1){
-		map[x+1][y-1] % 2 && ++count;
-		map[x+1][y]   % 2 && ++count;
-		map[x+1][y+1] % 2 && ++count;
-	}
+	map[x+1][y-1] % 2 && ++count;
+	map[x+1][y]   % 2 && ++count;
+	map[x+1][y+1] % 2 && ++count;
 	return count;
 }
 
-function evolve(){
-	function grow(x,y){
-		map[x][y] += 2;
+evolve = function(){
+	var grow = (x,y)=>{map[x][y] += 2}
+	var stay = (x,y)=>{map[x][y] && (map[x][y] = 3)}
+	return function(){
+			map.each((x,y)=>{
+			var p = pressure(x,y);
+			if(p==3){
+				grow(x,y);
+			}else if(p==2){
+				stay(x,y);
+			};
+		})
 	}
-	function stay(x,y){
-		map[x][y] && (map[x][y] = 3);
-	}
-	map.each((x,y)=>{
-		var p = pressure(x,y);
-		if(p==2){
-			grow(x,y);
-		}else if(p==3){
-			stay(x,y);
-		};
-	})
-} 
+}();
 
 function drawMap(){
 	map.each((x,y)=>{
-		map[x][y] = map[x][y] > 1 ? 1 : 0;
-	});
-	// flag = !flag;
-	if(flag){
-		map.each((x,y)=>{
-			drawPixel(x,y,map[x][y]);
-		})
-	}
+		let state = map[x][y];
+		if(state==1){
+			map[x][y] = 0;
+ 			flag && drawPixel(x,y,0);
+		}else if(state>1){
+			if(state==2){
+ 				flag && drawPixel(x,y,1);
+			}
+			map[x][y] = 1;	
+		};
+	})
 }
 
 function drawPixel(x,y,active){
-	ctx.fillStyle = active ? "white": "black";
+	ctx.fillStyle = active ? "white" : "black";
 	ctx.fillRect(x*P,y*P,P,P);
 }
 
@@ -146,11 +178,13 @@ function addPixel(e){
 	var x = e.pageX - c.offsetLeft;  
 	var y = e.pageY - c.offsetTop;
 	x = Math.floor(x/P);
+	x = Math.max(0,Math.min(x,wCount-1));
 	y = Math.floor(y/P);
+	y = Math.max(0,Math.min(y,hCount-1));
  	if(curP.x != x || curP.y != y){
 		curP.x = x;
 		curP.y = y;
-		map[x][y] = 1;//map[x][y] ? 0 : 1;
-		drawPixel(x,y,map[x][y]);
+		map[x][y] = 1;
+		drawPixel(x,y,1);
  	}
 }
