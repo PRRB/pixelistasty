465d234ba3d8bea8d98c8820ecad4cddc6ac5b52
phil <philip.barwood@gmail.com>
2018-04-29 22:10:10 +0100

    - Introduce noWrap setting and enable map wrapping - Put globals for map colors - Rename variables for readability - Ensure clock is stopped on reset

--- script.js
+++ script.js
@@ -1,79 +1,86 @@
 /* written by P.B. - 2017 */
 
-var b;
-var s;
-var c;
-var w;
-var ctx;
-var map;
-
+// Settings
 var P = 9; //pixel size
 var interval = 50; //tick interval millisecs
-var wCount;
-var hCount;
+var color1 = 'white';
+var color2 = 'black';
+var noWrap = false;
+
+// Globals
+var body;
+var slider;
+var canvas;
+var window;
+var ctx;
+var map;
+var wCount, xHigh;
+var hCount, yHigh;
 
 window.onload = function () {
-	w = window;
-	b = document.body;
-	b.style.overflow = "hidden";
-	var W = w.innerWidth - 18;
-	var H = w.innerHeight - 18;
+	window = window;
+	body = document.body;
+	body.style.overflow = "hidden";
+	let W = window.innerWidth - 18;
+	let H = window.innerHeight - 18;
 	wCount = Math.ceil(W / P) - 1;
 	hCount = Math.ceil(H / P) - 1;
-	c = document.createElement("canvas");
-	c.width = wCount * P;
-	c.height = hCount * P;
-	c.style.display = "block";
-	c.style.margin = "auto";
-	c.style.border = "1px solid #d3d3d3";
-	ctx = c.getContext("2d");
-	c.addEventListener('dblclick', onDblClick, true);
-	c.addEventListener('mousedown', mouseDown, true);
-	c.addEventListener('mouseup', mouseUp, true);
-	c.addEventListener('contextmenu', onRightClick, true);
-	c.addEventListener('touchstart', onTouch, false);
-	c.addEventListener('touchmove', onTouch, false);
-	c.addEventListener('touchend', onTouchEnd, false);
-	b.addEventListener('keydown', keyDown, true);
-	b.appendChild(c);
+	xHigh = wCount - 1;
+	yHigh = hCount - 1;
+	canvas = document.createElement("canvas");
+	canvas.width = wCount * P;
+	canvas.height = hCount * P;
+	canvas.style.display = "block";
+	canvas.style.margin = "auto";
+	canvas.style.border = "1px solid #d3d3d3";
+	ctx = canvas.getContext("2d");
+	canvas.addEventListener('dblclick', onDblClick, true);
+	canvas.addEventListener('mousedown', mouseDown, true);
+	canvas.addEventListener('mouseup', mouseUp, true);
+	canvas.addEventListener('contextmenu', onRightClick, true);
+	canvas.addEventListener('touchstart', onTouch, false);
+	canvas.addEventListener('touchmove', onTouch, false);
+	canvas.addEventListener('touchend', onTouchEnd, false);
+	body.addEventListener('keydown', keyDown, true);
+	body.appendChild(canvas);
 	initMap();
 	createSlider();
 	initSplash();
 }
 
 function createSlider() {
-	s = document.createElement("input");
-	s.type = "range";
-	s.min = 0;
-	s.max = 100;
-	s.value = 75;
-	s.style.position = "fixed";
-	s.style.bottom = "18px";
-	s.style.width = "80%";
-	s.style.marginLeft = "10%";
-	b.appendChild(s);
-	s.addEventListener('input', setInterval, true);
+	slider = document.createElement("input");
+	slider.type = "range";
+	slider.min = 0;
+	slider.max = 100;
+	slider.value = 75;
+	slider.style.position = "fixed";
+	slider.style.bottom = "18px";
+	slider.style.width = "80%";
+	slider.style.marginLeft = "10%";
+	body.appendChild(slider);
+	slider.addEventListener('input', setInterval, true);
 }
 
-function getId(id) {
+function getElemById(id) {
 	return document.getElementById(id);
 }
 
 function initSplash() {
-	var div = getId('splash');
-	div.style.left = (w.innerWidth - splash.offsetWidth) / 2 + 'px';
-	div.style.top = (w.innerHeight - splash.offsetHeight) / 2 + 'px';
+	var div = getElemById('splash');
+	div.style.left = (window.innerWidth - splash.offsetWidth) / 2 + 'px';
+	div.style.top = (window.innerHeight - splash.offsetHeight) / 2 + 'px';
 	div.addEventListener('click', function () {
 		div.style.visibility = 'hidden';
 	}, true);
-	getId('wiki').addEventListener('click', function (e) {
+	getElemById('wiki').addEventListener('click', function (e) {
 		e.stopPropagation();
 		e.cancelBubble = true;
 	}, true);
 }
 
 function keyDown(e) {
-	if (e.keyCode == 32) {
+	if (e.keyCode == 32) { //space
 		toggleClock();
 	}
 }
@@ -91,53 +98,55 @@ function onRightClick(e) {
 
 function onDblClick(e) {
 	reset(e);
-	getId('splash').style.visibility = 'visible';
+	getElemById('splash').style.visibility = 'visible';
 }
 
 function mouseDown(e) {
-	curP.x = -1;
-	curP.y = -1;
+	currentPos.x = -1;
+	currentPos.y = -1;
 	eAddPixel(e);
-	c.addEventListener('mousemove', eAddPixel, true);
+	canvas.addEventListener('mousemove', eAddPixel, true);
 }
 
 function mouseUp(e) {
-	c.removeEventListener('mousemove', eAddPixel, true);
+	canvas.removeEventListener('mousemove', eAddPixel, true);
 }
 
 function setInterval() {
-	if (s.value == s.min) {
+	if (slider.value == slider.min) {
 		interval = 1e16;
-		running = false;
+		isRunning = false;
 	} else {
-		interval = (s.max - s.value) * 5;
-		if (running) {
-			clearTimeout(cid);
-			cid = setTimeout(onTick, interval);
+		interval = (slider.max - slider.value) * 5;
+		if (isRunning) {
+			clearTimeout(timerId);
+			timerId = setTimeout(onTick, interval);
 		} else {
 			toggleClock();
 		}
 	}
 }
 
-var running;
-var cid;
-function toggleClock(state) {
-	if (interval == 1e16) return;
-	if (running) {
-		running = false;
-		clearTimeout(cid);
-	} else {
-		running = true;
+var isRunning;
+var timerId;
+function toggleClock(isReset) {
+	if (interval == 1e16) {
+		return;
+	}
+	if (isRunning) {
+		isRunning = false;
+		clearTimeout(timerId);
+	} else if (!isReset) {
+		isRunning = true;
 		onTick();
 	}
 }
 
 function onTick() {
-	if (running) {
+	if (isRunning) {
 		evolve();
 		drawMap();
-		cid = setTimeout(onTick, interval);
+		timerId = setTimeout(onTick, interval);
 	}
 }
 
@@ -170,52 +179,55 @@ function initMap() {
 }
 
 function reset(e) {
-	cid && toggleClock();
+	timerId && toggleClock(true);
 	map.each((x, y) => { map[x][y] = 1 });
 	drawMap();
 }
 
 function pressure(x, y) {
-	var count = 0;
-	if (y == 0 || y == hCount - 1 || x == 0 || x == wCount - 1) {
+	if (noWrap && (y === 0 || y === yHigh || x === 0 || x === xHigh)) {
 		return -1;
 	}
-	map[x - 1][y - 1] % 2 && ++count;
-	map[x - 1][y] % 2 && ++count;
-	map[x - 1][y + 1] % 2 && ++count;
-	map[x][y - 1] % 2 && ++count;
-	map[x][y + 1] % 2 && ++count;
-	map[x + 1][y - 1] % 2 && ++count;
-	map[x + 1][y] % 2 && ++count;
-	map[x + 1][y + 1] % 2 && ++count;
+	let count = 0;
+	let T = y === 0 ? yHigh : y - 1;
+	let B = y === yHigh ? 0 : y + 1;
+	let L = x === 0 ? xHigh : x - 1;
+	let R = x === xHigh ? 0 : x + 1;
+	map[L][T] % 2 && ++count;
+	map[L][y] % 2 && ++count;
+	map[L][B] % 2 && ++count;
+	map[x][T] % 2 && ++count;
+	map[x][B] % 2 && ++count;
+	map[R][T] % 2 && ++count;
+	map[R][y] % 2 && ++count;
+	map[R][B] % 2 && ++count;
 	return count;
 }
 
 var evolve = function () {
 	var grow = (x, y) => { map[x][y] += 2 }
 	var stay = (x, y) => { map[x][y] && (map[x][y] = 3) }
-	return function () {
+	return function evolve() {
 		map.each((x, y) => {
 			var p = pressure(x, y);
-			if (p == 3) {
+			if (p === 3) {
 				grow(x, y);
-			} else if (p == 2) {
+			} else if (p === 2) {
 				stay(x, y);
 			};
 		})
 	}
 }();
 
-var flag = true;
 function drawMap() {
 	map.each((x, y) => {
 		var state = map[x][y];
 		if (state == 1) {
 			map[x][y] = 0;
-			flag && drawPixel(x, y, 0);
+			drawPixel(x, y, 0);
 		} else if (state > 1) {
 			if (state == 2) {
-				flag && drawPixel(x, y, 1);
+				drawPixel(x, y, 1);
 			}
 			map[x][y] = 1;
 		};
@@ -223,30 +235,25 @@ function drawMap() {
 }
 
 function drawPixel(x, y, active) {
-	ctx.fillStyle = active ? "white" : "black";
+	ctx.fillStyle = active ? color1 : color2;
 	ctx.fillRect(x * P, y * P, P, P);
-}
-
-function getSq(x, y) {
-	x -= x % P;
-	y -= y % P;
 }
 
 function eAddPixel(e) {
 	addPixel(e.pageX, e.pageY);
 }
 
-var curP = { x: -1, y: -1 }
+var currentPos = { x: -1, y: -1 }
 function addPixel(pageX, pageY) {
-	var x = pageX - c.offsetLeft;
-	var y = pageY - c.offsetTop;
+	var x = pageX - canvas.offsetLeft;
+	var y = pageY - canvas.offsetTop;
 	x = Math.floor(x / P);
-	x = Math.max(0, Math.min(x, wCount - 1));
+	x = Math.max(0, Math.min(x, xHigh));
 	y = Math.floor(y / P);
-	y = Math.max(0, Math.min(y, hCount - 1));
-	if (curP.x != x || curP.y != y) {
-		curP.x = x;
-		curP.y = y;
+	y = Math.max(0, Math.min(y, yHigh));
+	if (currentPos.x != x || currentPos.y != y) {
+		currentPos.x = x;
+		currentPos.y = y;
 		map[x][y] = 1;
 		drawPixel(x, y, 1);
 	}
